df799b130516e246839313a22d173df7c042c620
liuqiuchen <2016968116@qq.com>
2016-04-27 14:55:29 +0800

    1

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+	$('#fullpage').fullpage({
+		sectionsColor: ['#ADD8E6', '#D3D3D3', '#F08080', '#FAFAD2'],
+		anchors: ['page1', 'page2', 'page3', 'page4'],
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['优秀四月新番总览表', '在下版本，有何贵干？', '甲铁城的卡巴内瑞', '12岁。']
+
+		// events
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
